028b539c94f091c01debfaa0f28ac578131c2c3b
Yusuf Sezgin <sezgin58yusuf34@gmail.com>
2023-02-10 23:26:18 +0300

    avatar and width changed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
-import image from '../../img/avatar.jpeg'
+import image from '../../img/avatar2.jpeg'
 
 function About() {
   return (
